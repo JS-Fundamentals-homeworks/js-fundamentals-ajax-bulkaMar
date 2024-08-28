6b747641f1bde6437e9156b2451612a9f3a17a1c
Маріна Булах <marinabulah@MacBook-Pro-Marina.local>
2024-08-28 20:21:56 +0300

    all task done

--- task2.js
+++ task2.js
@@ -5,3 +5,21 @@
 // відобразити у тезі #userCity
 // Запустити програму потрібно за допомогою Live Server
 // Перевірити правильність програми - команда node tests/task2.test.js
+
+let userNameInput = document.querySelector("#userNameInput");
+let getUserButton = document.querySelector("#getUserButton");
+let userCity = document.querySelector("#userCity");
+
+getUserButton.addEventListener("click", function () {
+  let userInputValue = userNameInput.value;
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response) => response.json())
+    .then((users) => {
+      for (let i in users) {
+        if (userInputValue == users[i].name) {
+          userCity.innerHTML = `${users[i].address.city}`;
+          break;
+        }
+      }
+    });
+});
